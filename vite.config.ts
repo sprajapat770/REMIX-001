63bd6ccede1bf90628822e4118fb0125e3bfe1ee
Suraj Prajapat <sprajapat770@gmail.com>
2024-03-16 00:59:41 +0530

    configuration issue fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: '/REMIX-001/',
   plugins: [
     remix({
+      basename: '/REMIX-001/',
       ssr: false,
     }),
     tsconfigPaths(),
